4742afc5a70dbf3012290a9faaef01adb25279eb
yasminehz <173774288+yasminehz@users.noreply.github.com>
2026-06-30 13:55:57 +0200

    css restore

--- react-portfolio/next.config.ts
+++ react-portfolio/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/mon_portfolio",
+  assetPrefix: "/mon_portfolio",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
